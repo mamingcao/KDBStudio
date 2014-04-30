df8d19c78dd6eb06dc6e5180210f8a5e339c6d8d
mingcao ma <mamingcao@gmail.com>
2014-05-01 00:22:05 +0800

    init

---
+++ build/classes/org/openide/loaders/DataLoaderPool.rs
@@ -0,0 +1,6 @@
+org.openide.loaders.DataLoaderPool$ShadowLoader
+org.openide.loaders.DataLoaderPool$DefaultLoader
+org.openide.loaders.DataLoaderPool
+org.openide.loaders.DataLoaderPool$FolderLoader
+org.openide.loaders.DataLoaderPool$InstanceLoader
+org.openide.loaders.DataLoaderPool$FolderLoader$1NodeSharingDataFolder
